54910d43b4ba55ff8a4cc2b0be38b62c5a259fba
Aditi Joshi <aditisjoshi14@gmail.com>
2019-07-09 15:02:03 +0530

    1

---
+++ JDBCcon_java/src/JdbcDemo.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+
+public class JdbcDemo {
+	public static void main(String args[]) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "root", "Sapient123");
+			Statement stmnt = conn.createStatement();
+			
+			//stmnt.executeUpdate("insert into EMP values('xyz',23)");
+			//stmnt.executeUpdate("delete from EMP where age>25");
+			stmnt.executeUpdate("update EMP set AGE=AGE+1");
+			ResultSet rs = stmnt.executeQuery("select * from emp");
+			while (rs.next()) {
+				System.out.println("Name: "+rs.getString(1));
+				System.out.println("Age: "+rs.getInt(2));
+				System.out.println("-------------------");
+			}
+			rs.close();
+			stmnt.close();
+			conn.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
